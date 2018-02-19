57342d6a995733a664e39280317f4682df3aad00
hsun0120 <hsun960120@gmail.com>
2018-02-19 15:29:15 -0800

    Add shell script for batch processing

---
+++ src/stmData/searchKTest.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Rscript ./searchKTest.R S 1week
+Rscript ./searchKTest.R V 1week
+Rscript ./searchKTest.R O 1week
+Rscript ./searchKTest.R SV 1week
+Rscript ./searchKTest.R VO 1week
+Rscript ./searchKTest.R SO 1week
