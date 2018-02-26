d1a60ea89796317d7bc2b4110dd72ac413677baf
hsun0120 <hsun960120@gmail.com>
2018-02-25 16:42:48 -0800

    Add TableReader to preprocess data retrieved from postgres.

---
+++ src/DemoParser.java
@@ -0,0 +1,7 @@
+/**
+ * A class for demo phrase extraction using Stanford Parser and HanLP
+ * @author Haoran Sun
+ * @since 02-25-2018
+ */
+public class DemoParser {
+}
---
+++ src/utils/TableReader.java
@@ -0,0 +1,29 @@
+package utils;
+
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+public class TableReader {
+	public static void tableToFiles(String table, String output) {
+		try(CSVReader reader = new CSVReader((new InputStreamReader(new 
+				FileInputStream(table), StandardCharsets.UTF_8)))) {
+			String row[] = null;
+			while((row = reader.readNext()) != null) {
+				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new 
+						FileOutputStream(output + "/" + row[0] + ".txt"), 
+						StandardCharsets.UTF_8));
+				writer.write(row[6]); //Write original text
+				writer.close();
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
